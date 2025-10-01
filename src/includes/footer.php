460fd901721dd41c1791927231309af0c1fe3c21
LenonWolf <thewolfplayer2004@gmail.com>
2025-10-01 12:55:47 -0600

    Actualización validación y estilo footer

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -4,4 +4,20 @@
     <p><i class="fas fa-code"></i> Desarrollado por Juan Alberto Sanchez Hernandez y Luis Eduardo Nieves Avila</p>
     <p><i class="fas fa-info-circle"></i> Sistema v1.0 | <a href="https://lenonwolf.github.io/Assets_Tollan_Le_Funk/docs/Manual.pdf" target="_blank" rel="noopener noreferrer">Manual de uso</a></p>
     <p>&copy;<?php echo date("Y"); ?> <b>Tollan le Funk</b> - Todos los Derechos Reservados.</p>
+    <div id="validator">
+        <p>
+            <a href="https://jigsaw.w3.org/css-validator/check/referer">
+                <img style="border:0;width:88px;height:31px"
+                    src="https://jigsaw.w3.org/css-validator/images/vcss"
+                    alt="¡CSS Válido!">
+            </a>
+        </p>
+        <p>
+            <a href="https://jigsaw.w3.org/css-validator/check/referer">
+                <img style="border:0;width:88px;height:31px"
+                    src="https://jigsaw.w3.org/css-validator/images/vcss-blue"
+                    alt="¡CSS Válido!">
+            </a>
+        </p>
+    </div>
 </footer>
